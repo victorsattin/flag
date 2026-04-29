9660873a61109902d2516d87408ea82206cd0a13
victorsattin <victorvsattin@gmail.com>
2026-04-28 22:56:19 -0300

    OpenCV.py

--- OpenCV.py
+++ OpenCV.py
@@ -1,35 +1,55 @@
-#OpenCV
+# OpenCV
 
 import os
 import cv2
 
 video_folder = "videos"
-output_folder = "dataset"  
+output_folder = "dataset"
 
 os.makedirs(output_folder, exist_ok=True)
 
-frame_interval = 120  
+frame_interval = 120
 count = 0
 
 for video_name in os.listdir(video_folder):
-    video_path = os.path.join(video_folder, video_name)
-    
+
+    video_path = os.path.join(
+        video_folder,
+        video_name
+    )
+
     cap = cv2.VideoCapture(video_path)
+
     frame_id = 0
-   
+
     while True:
+
         ret, frame = cap.read()
+
         if not ret:
             break
-        
+
         if frame_id % frame_interval == 0:
-            frame_resized = cv2.resize(frame, (1920, 1080))
-            file_name = os.path.join(output_folder, f"img_{count}.jpg")
-            cv2.imwrite(file_name, frame)
+
+            frame_resized = cv2.resize(
+                frame,
+                (1920, 1080)
+            )
+
+            file_name = os.path.join(
+                output_folder,
+                f"img_{count}.jpg"
+            )
+
+            cv2.imwrite(
+                file_name,
+                frame_resized
+            )
+
             count += 1
-        
+
         frame_id += 1
 
     cap.release()
 
-print("Frames extraídos com sucesso")
+print("Frames extracted successfully")
